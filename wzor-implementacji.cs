494aea10d2b90fe76238da97b73511c67d8f753a
Michał <kremuwa@gmail.com>
2014-04-26 13:15:00 +0200

    Wrzucony projekt i wzory kontraktów

---
+++ wzor-implementacji.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serviceRepository = "net.tcp://localhost:54321/ServiceRepo";//to powinno byc odczytane z app.config
+            //strworzenie clienta do service repo
+            //zarejestrowanie swojej uslugi
+            //odpalenie timera w celu wysylania Alive
+            //stworznie AccountRepository i przekazanie mu serviceRepository zeby odpytac o potrzebne sewisy
+
+            // logowanie
+        }
+    }
+
+    public class AccountRepository : IAccountRepository
+    {
+        public Account CreateAccount(Guid clientId)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ wzor1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Contracts
+{
+	[ServiceContract]
+    public interface IServiceRepository
+    {
+        void RegisterService(string serviceName, string serviceAddress);
+        void UnregisterService(string serviceName);
+
+        string GetServiceAddress(string serviceName);
+
+        void IsAlive(string serviceName);
+    }
+}
