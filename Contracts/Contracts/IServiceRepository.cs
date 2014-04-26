ac7b851d29320256f3d03186f1086921c7b69bfa
mastablasta1 <tomasz.idziak@gmail.com>
2014-04-27 00:55:45 +0200

    Ludzie, ogarnijcie to w koncu

---
+++ Contracts/Contracts/IServiceRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IServiceRepository
+    {
+        void RegisterService(string serviceName, string serviceAddress);
+        void UnregisterService(string serviceName);
+
+        string GetServiceAddress(string serviceName);
+
+        void IsAlive(string serviceName);
+    }
+}
