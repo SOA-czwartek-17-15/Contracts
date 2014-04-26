f34520c52ea0c5669a9ab4e25d99b34a635fc71d
mastablasta1 <tomasz.idziak@gmail.com>
2014-04-27 00:44:28 +0200

    Wstepne kontrakty. Potrzebne do ICanTransferMoney.

---
+++ Contracts/Contracts/IAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IAccountRepository
+    {
+        [OperationContract]
+        long createAccount(int clientId, Account details);
+
+        [OperationContract]
+        Account getAccountInformation(long accountNumber);
+    }
+}
