605a021f474b2ac610e2984bc0cc3a3b35c582dc
michalkurzeja <michal.kurzeja@polcode.net>
2014-04-27 10:24:52 +0200

    IAuditorService + Audit

---
+++ Contracts/Contracts/Audit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Contracts
+{
+    [DataContract]
+    public class Audit
+    {
+        public Guid Id { get; set; }
+        [DataMember]
+        public int AccountId { get; set; }
+        [DataMember]
+        public long Money { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Contracts/Contracts/IAuditorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IAuditorService
+    {
+        [OperationContract]
+        int GetNumberOfTransfersByDate(DateTime date);
+        [OperationContract]
+        int GetTransferedMoneyByDate(DateTime date);
+        [OperationContract]
+        int GetCreditNumber();
+        [OperationContract]
+        IEnumerable<Audit> AuditAll();
+        [OperationContract]
+        IEnumerable<Audit> GetAuditsByDate(DateTime date);
+        [OperationContract]
+        IEnumerable<Audit> GetAuditsByAccount(int accountId);
+        [OperationContract]
+        bool AddAudit(int accountId, long Money);
+    }
+}
