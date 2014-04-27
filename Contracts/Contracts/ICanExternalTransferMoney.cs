d0c34dafeebc0c6acf4e9129f49c768f9c98413e
Mateusz Kotlarz <kotlarz.mateusz@gmail.com>
2014-04-27 20:03:03 +0200

    Przelewy zewnętrzne dodane do kontraktu

---
+++ Contracts/Contracts/ICanExternalTransferMoney.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Contracts
+{
+	/*
+	* Przelewy zewnętrzne
+	**/
+    [ServiceContract]
+    public interface ICanExternalTransferMoney
+    {
+        [OperationContract]
+        Guid ReceiveExternalMoney(Guid from, string to, double howMany);
+        [OperationContract]
+        Guid SendExternalMoney(string from, Guid to, double howMany);
+    }
+}
