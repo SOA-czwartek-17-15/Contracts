27abf37902b008b132752168b357719b7748884b
Tomasz Idziak <tomasz.idziak@gmail.com>
2014-05-10 22:22:40 +0200

    Niezbedne/sugerowane zmiany

--- Contracts/Contracts/Account.cs
+++ Contracts/Contracts/Account.cs
@@ -9,22 +9,23 @@ namespace Contracts
     [DataContract]
     public class Account
     {
+        // zakomentowane pola chyba nie sa zwiazane z kontem, ale klientem
         public Guid id { get; set; }
         public string accountNumber { get; set; }
         [DataMember]
         public int clientId { get; set; }
-        [DataMember]
-        public string firstName { get; set; }
-        [DataMember]
-        public string lastName { get; set; }
+        //[DataMember]
+        //public string firstName { get; set; }
+        //[DataMember]
+        //public string lastName { get; set; }
         [DataMember]
         public long money { get; set; }
-        [DataMember]
-        public string pesel { get; set; }
-        [DataMember]
-        public string address { get; set; }
-        [DataMember]
-        public string phoneNumber { get; set; }
+        //[DataMember]
+        //public string pesel { get; set; }
+        //[DataMember]
+        //public string address { get; set; }
+        //[DataMember]
+        //public string phoneNumber { get; set; }
         [DataMember]
         public string type { get; set; }
         [DataMember]
--- Contracts/Contracts/IAccountRepository.cs
+++ Contracts/Contracts/IAccountRepository.cs
@@ -14,5 +14,12 @@ namespace Contracts
 
         [OperationContract]
         Account GetAccountInformation(string accountNumber);
+
+        [OperationContract]
+        Account GetAccountById(string accountId);
+
+        // amount to kwota ktora ma zostac dodana/odjeta od konta (w zaleznosci od znaku)
+        [OperationContract]
+        bool ChangeAccountBalance(string accountNumber, long amount);
     }
 }
