ac5f68c92fea121cec9b33673efae3d8c7f9a797
Michał <kremuwa@gmail.com>
2014-05-21 23:58:23 +0200

    Deposit - dodanie namespace'u Contracts

--- Contracts/Contracts/Deposit.cs
+++ Contracts/Contracts/Deposit.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-[DataContract]
-public class Deposit
+namespace Contracts
 {
-    [DataMember]
-    public Guid Id { get; set; }
-    [DataMember]
-    public Guid ClientId { get; set; }
-    [DataMember]
-    public string AccountNumber { get; set; }
-    [DataMember]
-    public string Type { get; set; }
-    [DataMember]
-    public double Interest { get; set; }
-    [DataMember]
-    public int CapitalizationPeriod { get; set; }
-    [DataMember]
-    public double Money { get; set; }
-    [DataMember]
-    public string OpeningDate { get; set; }
-    [DataMember]
-    public string Duration { get; set; }
+	[DataContract]
+	public class Deposit
+	{
+		[DataMember]
+		public Guid Id { get; set; }
+		[DataMember]
+		public Guid ClientId { get; set; }
+		[DataMember]
+		public string AccountNumber { get; set; }
+		[DataMember]
+		public string Type { get; set; }
+		[DataMember]
+		public double Interest { get; set; }
+		[DataMember]
+		public int CapitalizationPeriod { get; set; }
+		[DataMember]
+		public double Money { get; set; }
+		[DataMember]
+		public string OpeningDate { get; set; }
+		[DataMember]
+		public string Duration { get; set; }
+	}
 }
