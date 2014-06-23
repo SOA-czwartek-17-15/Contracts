5cbabd5a8d1bb2b62b2b8ea0d19f1d4691b5174b
Michał <kremuwa@gmail.com>
2014-06-23 08:53:45 +0200

    Message do IDepositRepository

--- Contracts/Contracts/IDepositRepository.cs
+++ Contracts/Contracts/IDepositRepository.cs
@@ -3,14 +3,52 @@ using System;
 
 namespace Contracts
 {
+    // ogólna wiadomość dla serwisu DepositRepository
+
+    public class DepositRepositoryMessage
+    {
+        public string Action { get; set; }
+        public string Arguments { get; set; }
+    }
+
+    // argumenty dla wiadomości odpowiadającej metodzie CreateDeposit dla id klienta
+
+    public class CreateDepositForClientMessage
+    {
+        public Guid ClientId { get; set; }
+        public Deposit Deposit { get; set; }
+    }
+
+    // argumenty dla wiadomości odpowiadającej metodzie CreateDeposit dla numeru konta
+
+    public class CreateDepositForAccountMessage
+    {
+        public String AccountNumber { get; set; }
+        public Deposit Deposit { get; set; }
+    }
+
+    // argumenty dla wiadomości odpowiadającej metodzie GetDepositInformation
+
+    public class GetDepositInformationMessage
+    {
+        public Guid DepositId { get; set; }
+    }
+
+    // argumenty dla wiadomości odpowiadającej metodzie RemoveDeposit
+
+    public class RemoveDepositMessage
+    {
+        public Guid DepositId { get; set; }
+    }
+
     [ServiceContract]
     public interface IDepositRepository
     {
-        [OperationContract(Name="CreateDepositForClient")]
-        Guid CreateDeposit(Guid clientId, Deposit deposit);
+		[OperationContract]
+        Guid CreateDepositForClient(Guid clientId, Deposit deposit);
 
-        [OperationContract(Name="CreateDepositForAccount")]
-        Guid CreateDeposit(string accountNumber, Deposit deposit);
+		[OperationContract]
+        Guid CreateDepositForAccount(string accountNumber, Deposit deposit);
 
         [OperationContract]
         Deposit GetDepositInformation(Guid depositId);
